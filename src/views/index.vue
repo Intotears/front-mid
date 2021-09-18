64ef6ae65857ccda3cf3b8558e6f392e9d0693ee
Yokymoth <57820964+Yokymoth@users.noreply.github.com>
2021-09-18 18:52:07 +0700

    แก้หน้า index ที่กดแล้วไม่ลิงก์ไปหน้า login และ register

--- src/views/index.vue
+++ src/views/index.vue
@@ -9,12 +9,12 @@
 
       <v-row align-self="center" justify="center">
         <v-col cols="12" xs="12" sm="5" md="4" lg="4">
-          <v-btn color="primary" block x-large depressed to="/Login">
+          <v-btn color="primary" block x-large depressed to="/login">
             Signin
           </v-btn>
         </v-col>
         <v-col cols="12" xs="12" sm="5" md="4" lg="4">
-          <v-btn color="primary" block x-large depressed to="/Register"
+          <v-btn color="primary" block x-large depressed to="/register"
             >Register
           </v-btn>
         </v-col>
